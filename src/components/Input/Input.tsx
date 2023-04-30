9c071f58bf0d44c3c119dfde0ed7412afeacbd2c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-30 21:25:19 +0300

    feat: add universal input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,26 @@
+import React, {ChangeEvent, useState} from 'react';
+
+import s from './Input.module.css'
+
+type PropsType = {
+    value: string
+    callBack: (e: string) => void
+    className?: string
+    type?: string
+}
+
+export const Input: React.FC<PropsType> = (props) => {
+    const {value, callBack, type='number', ...restProps} = props
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        callBack(e.currentTarget.value)
+    }
+
+    const inputClasses = `
+    ${s.default}
+    ${restProps.className}
+    `
+    return (
+        <input value={value} onChange={onChangeHandler} type={type} className={inputClasses} />
+    )
+}
